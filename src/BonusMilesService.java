c4d11eae1c49e1b39b107874af45a1b076726a4f
Kipiatkova <kipiatkova1998@mail.ru>
2021-12-09 00:23:37 +0300

    Added new

--- src/BonusMilesService.java
+++ src/BonusMilesService.java
@@ -1,7 +1,7 @@
 public class BonusMilesService {
     public int calculate(int cost) {
-        int price = 10_000;
-        int miles = price / 20;
+        int price = cost;
+        int miles = cost / 20;
         return miles;
     }
 }
